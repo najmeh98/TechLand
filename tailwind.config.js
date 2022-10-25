afa2d180ea529d11b72c0f73e8af614e104f9e16
najmeh98 <foroughi.najmeh98@gmail.com>
2022-10-25 12:37:43 +0330

    Add package

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      maxsm: { max: "480px" },
+      maxlg: { max: "976px" },
+      maxmd: { max: "768px" },
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1200px",
+    },
+
     extend: {
       colors: {
         extra_text: "rgba(117, 117, 117, 1)",
+        bg_gradient:
+          "linear-gradient(120deg, #e5e5e5 30%, #f0f0f0 38%, #f0f0f0 40%, #e5e5e5 48%)",
       },
+
       fontSize: {
         h1: "42px",
       },
