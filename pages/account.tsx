d77e8199c149ee491356e8127723b40cede4f2c8
najmeh98 <foroughi.najmeh98@gmail.com>
2022-10-25 12:44:38 +0330

    Add banner image to account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -31,15 +31,24 @@ export default function Account(): JSX.Element {
       className="max-w-[60rem]   min-h-screen w-full flex flex-col border border-r border-l border-slate-300 mx-auto  whitespace-normal"
     >
       {/* main image */}
-      <div className="border  border-solid border-slate-100  bg-white rounded-3xl shadow-md">
-        <img
-          src="img.jpeg"
-          alt={data?.name}
-          height={300}
-          className="w-full rounded-b-3xl"
-        />
+      <div className="border mx-auto  container border-solid border-slate-100  bg-white rounded-3xl shadow-md">
+        {data?.banner == null ? (
+          <img
+            src="img.jpeg"
+            alt={data?.name}
+            height={300}
+            className="w-full rounded-b-3xl"
+          />
+        ) : (
+          <img
+            src={data?.banner}
+            alt={data?.name}
+            height={300}
+            className="w-full rounded-b-3xl"
+          />
+        )}
 
-        <div className="max-w-[47rem] mx-auto flex flex-col  justify-start  w-full ">
+        <div className="lg:max-w-[47rem] md:max-w-[42rem] mx-auto flex flex-col  justify-start  w-full  sm:max-w-[25rem] ">
           {/* main text */}
           <div className="flex items-start justify-start w-full  ">
             <img
@@ -47,10 +56,10 @@ export default function Account(): JSX.Element {
               alt={data?.name}
               width={130}
               height={130}
-              className="rounded-full shadow-2xl relative -top-10 border-4 border-solid border-white  "
+              className="rounded-full shadow-2xl relative -top-10 border-4 border-solid border-white   maxmd:w-24 maxmd:h-24"
             />
             <div className="flex flex-col items-start justify-between  pl-5 ">
-              <ThemedText className="text-3xl  pt-4 capitalize font-medium">{`${data?.name} ${data?.family}`}</ThemedText>
+              <ThemedText className="text-3xl  pt-4 capitalize font-medium maxmd:text-2xl maxmd:pt-2">{`${data?.name} ${data?.family}`}</ThemedText>
               <ThemedText className="text-extra_text pt-1 text-base font-mono">{`@${data?.name}`}</ThemedText>
 
               {/* <Space vertical={20} /> */}
@@ -72,8 +81,8 @@ export default function Account(): JSX.Element {
       <Space vertical={20} />
 
       {data && data?.bio.length > 0 && (
-        <div className=" flex flex-col w-full h-96 items-center justify-start rounded-3xl border border-solid border-slate-100  shadow-md bg-white">
-          <div className="max-w-[47rem] py-7">
+        <div className=" flex container  mx-auto flex-col w-full h-96 items-center justify-start rounded-3xl border border-solid border-slate-100  shadow-md bg-white">
+          <div className="lg:max-w-[47rem] md:max-w-[42rem] py-7 sm:max-w-[25rem] ">
             <ThemedText className="font-sans font-bold text-xl leading-5 ">
               About Me
             </ThemedText>
